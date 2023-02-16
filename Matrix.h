997a44169e32d706a53cab3ecf87198aaafef984
Danil <123>
2023-02-16 22:58:32 +0300

    подправил

--- Matrix.h
+++ Matrix.h
@@ -3,7 +3,7 @@ class Matrix {
 private:
 	int rows;
 	int columns;
-	float* matrix; // придётся делать массив указателей на массив типа float :(
+	float* matrix; // придётся делать массив указателей на массив указателей типа float :(
 public:
 	Matrix(int rows, int columns) {
 		this->rows = rows;
