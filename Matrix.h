8b9550f0aa34623eb3b0ffb0a7dbad69532a8e9e
Danil <123>
2023-02-18 11:50:10 +0300

    деструктор и конструктор двумерного массива

--- Matrix.h
+++ Matrix.h
@@ -3,10 +3,28 @@ class Matrix {
 private:
 	int rows;
 	int columns;
-	float* matrix; // придётся делать массив указателей на массив указателей типа float :( dq
+	float** matrix; // придётся делать массив указателей на массив указателей типа float :( dq
 public:
-	Matrix(int rows, int columns) {
+	Matrix(int rows, int columns) 
+	{
 		this->rows = rows;
 		this->columns = columns;
+		matrix = new float*[rows];
+		for (int b = 0; b < rows; b++)
+		{
+			matrix[b] = new float[rows];
+			for (int i = 0; i < columns; i++)
+			{
+				matrix[i][b] = {0};
+			}
+		}
+	}
+	~Matrix()
+	{
+		for (int i = 0; i < rows; i++)
+		{
+			delete[] matrix[i];
+		}
+		delete[] matrix;
 	}
 };
